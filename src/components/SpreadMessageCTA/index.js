1b7050e2442d97f9b145019df0deb4df959f32c3
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-04-30 14:03:17 +0100

    initial file structure

---
+++ src/components/SpreadMessageCTA/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export default function SpreadMessageCTA() {
+  return (
+    <div className={styles.container}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </div>
+    //  a container with
+    // two internal components: SpreadMessage & Social Container;
+  );
+}
