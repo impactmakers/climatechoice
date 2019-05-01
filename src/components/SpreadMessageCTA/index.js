1567d43c345ec7faace5ddf911645867d493f188
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-05-01 09:52:30 +0100

    adding content to cta
    TODO:
    - move inline styling to css file
    - utilise scss for global styles
    - responsive work
    - styling tidy up

--- src/components/SpreadMessageCTA/index.js
+++ src/components/SpreadMessageCTA/index.js
@@ -1,23 +1,58 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import GlobalStyles from "../../styles/Global.module.scss";
+import SocialButton from "../SocialButton";
+
+const socialButtons = [
+  {
+    description: "Twitter"
+  },
+  {
+    description: "Facebook"
+  },
+  {
+    description: "WhatsApp"
+  },
+  {
+    description: "LinkedIn"
+  },
+  {
+    description: "Email"
+  }
+];
 
 export default function SpreadMessageCTA() {
   return (
     <div className={GlobalStyles.inner}>
       <div className={styles.container}>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <div className={styles.ctaCopy}>
+          <div className={styles.title}>Spread the Message!</div>
+          <p className={styles.copy}>
+            Don't assume everyone knows they should change the way that they eat
+            to revent climate breakdown. We need everyone to become informed so
+            they can also make a choice about what they eat.
+          </p>
+        </div>
+        <div className={styles.socialBlock}>
+          <h3 className={styles.subTitle}>
+            Want to let others know how they can change what they eat?
+          </h3>
+          <p style={{ maxWidth: "80%", color: "#0d1626", lineHeight: "1.5em" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          </p>
+          <div
+            style={{ display: "flex", justifyContent: "center", width: "80%" }}
+          >
+            {socialButtons.map(item => (
+              <div style={{ margin: "0 0.5em" }}>
+                <SocialButton description={item.description} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-    //  a container with
-    // two internal components: SpreadMessage & Social Container;
   );
 }
