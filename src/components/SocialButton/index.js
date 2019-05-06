80686b407ad675a23e0487a9b692d9026d80cf7f
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-05-06 14:24:21 +0100

    add hover state on social buttons

--- src/components/SocialButton/index.js
+++ src/components/SocialButton/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./style.module.scss";
+import buttonStyles from "../../styles/Buttons.module.scss";
 
 export default function SocialButton({ logo, description }) {
   return (
-    <div className={styles.button}>
-      <div className={styles.logo}>{logo}</div>
-      {description}
-    </div>
+    <a>
+      <div className={`${styles.button} ${buttonStyles.btnSimple}`}>
+        <div className={styles.logo}>{logo}</div>
+        {description}
+      </div>
+    </a>
   );
 }
 
