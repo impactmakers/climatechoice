a8967c46b3ef5326ebe70f994589d9e9b89e4e59
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-05-07 09:45:21 +0100

    add twitter pop out from Twitter Button

--- src/components/SocialButtons/TwitterButton.js
+++ src/components/SocialButtons/TwitterButton.js
@@ -1,22 +1,30 @@
 import React from "react";
+import { TwitterShareButton } from "react-share";
 import styles from "./styles.module.scss";
 import buttonStyles from "../../styles/Buttons.module.scss";
 import TwitterIcon from "../Images/Icons/TwitterIcon";
 
 export default function TwitterButton() {
   return (
-    <a
-      style={{ textDecoration: "none" }}
-      href="https://twitter.com/climatechoice"
-      target="_blank"
-    >
-      <div className={`${styles.button} ${buttonStyles.btnSimple}`}>
-        <input type="hidden" id="hidden-input" value={document.location.href} />
-        <div className={styles.logo}>
-          <TwitterIcon fillColor="#76a9ea" />
+    <>
+      <TwitterShareButton
+        url="https://climatechoice.co"
+        title="Check out how you can change your diet and positively affect climate change at"
+        via="climatechoice"
+        hashtags={["climatechange"]}
+      >
+        <div className={`${styles.button} ${buttonStyles.btnSimple}`}>
+          <input
+            type="hidden"
+            id="hidden-input"
+            value={document.location.href}
+          />
+          <div className={styles.logo}>
+            <TwitterIcon fillColor="#76a9ea" />
+          </div>
+          Twitter
         </div>
-        Twitter
-      </div>
-    </a>
+      </TwitterShareButton>
+    </>
   );
 }
