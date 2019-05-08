89cc175fe9034528f06914399d346d4e79f787c9
Paul McBride <hello@paulmcbride.net>
2019-05-08 19:54:47 +0100

    start work on mobile nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,64 +1,74 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Logo from '../Images/Logos/LogoOnDark';
-import GlobalStyles from '../../styles/Global.module.scss';
-import ButtonStyles from '../../styles/Buttons.module.scss';
-import Styles from './Styles.module.scss';
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import Logo from "../Images/Logos/LogoOnDark";
+import GlobalStyles from "../../styles/Global.module.scss";
+import ButtonStyles from "../../styles/Buttons.module.scss";
+import Styles from "./Styles.module.scss";
 
-const Navbar = class extends React.Component {
-  render() {
-    return (
-      <nav
-        className={Styles.container}
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className={`${GlobalStyles.inner} ${Styles.inner}`}>
-          <div className={Styles.logo}>
-            <Link to="/" title="Go back home">
-              <Logo />
-            </Link>
-          </div>
+function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
 
-          <ul className={Styles.navList}>
-            <li className={Styles.navItem}>
-              <Link to="/resources" title="Discover climate change resources">
-                Resources
-              </Link>
-            </li>
-            <li className={Styles.navItem}>
-              <Link
-                to="/contact#contribute"
-                title="Contribute to ClimateChoice"
-              >
-                Contribute
-              </Link>
-            </li>
-            <li className={Styles.navItem}>
-              <Link to="/donate" title="Donate to climate causes">
-                Donate
-              </Link>
-            </li>
-            <li className={`${Styles.navItem} ${Styles.navItemAction}`}>
-              <Link
-                to="#your-choices"
-                className={Styles.ctaButton}
-                title="Secondary"
-              >
-                <button
-                  className={`${ButtonStyles.btnSimple} ${ButtonStyles.btnSm} ${
-                    ButtonStyles.btnHeader
-                  } `}
-                >
-                  View your choices
-                </button>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+  function toggleNav(e) {
+    e.preventDefault();
+    setNavOpen(prevValue => !prevValue);
   }
-};
+
+  console.log(navOpen);
+
+  return (
+    <nav
+      className={Styles.container}
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <div className={`${GlobalStyles.inner} ${Styles.inner}`}>
+        <div className={Styles.logo}>
+          <Link to="/" title="Go back home">
+            <Logo />
+          </Link>
+        </div>
+
+        <button onClick={toggleNav} className={Styles.navToggle}>
+          Menu
+        </button>
+
+        <ul
+          className={`${Styles.navList} ${navOpen ? Styles.navListOpen : ""}`}
+        >
+          <li className={Styles.navItem}>
+            <Link to="/resources" title="Discover climate change resources">
+              Resources
+            </Link>
+          </li>
+          <li className={Styles.navItem}>
+            <Link to="/contact#contribute" title="Contribute to ClimateChoice">
+              Contribute
+            </Link>
+          </li>
+          <li className={Styles.navItem}>
+            <Link to="/donate" title="Donate to climate causes">
+              Donate
+            </Link>
+          </li>
+          <li className={`${Styles.navItem} ${Styles.navItemAction}`}>
+            <Link
+              to="#your-choices"
+              className={Styles.ctaButton}
+              title="Secondary"
+            >
+              <button
+                className={`${ButtonStyles.btnSimple} ${ButtonStyles.btnSm} ${
+                  ButtonStyles.btnHeader
+                } `}
+              >
+                View your choices
+              </button>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
