1198630b2a3bcdcf827f05915a01a07139daf4e7
Paul McBride <hello@paulmcbride.net>
2019-05-09 19:17:03 +0100

    remove logging

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Link } from 'gatsby';
-import Logo from '../Images/Logos/LogoOnDark';
-import MenuIcon from '../Images/Icons/MenuIcon';
-import GlobalStyles from '../../styles/Global.module.scss';
-import ButtonStyles from '../../styles/Buttons.module.scss';
-import Styles from './Styles.module.scss';
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import Logo from "../Images/Logos/LogoOnDark";
+import MenuIcon from "../Images/Icons/MenuIcon";
+import GlobalStyles from "../../styles/Global.module.scss";
+import ButtonStyles from "../../styles/Buttons.module.scss";
+import Styles from "./Styles.module.scss";
 
 function Navbar() {
   const [navOpen, setNavOpen] = useState(false);
@@ -13,8 +13,6 @@ function Navbar() {
     e.preventDefault();
     setNavOpen(prevValue => !prevValue);
   }
-
-  console.log(navOpen);
 
   return (
     <nav
@@ -38,7 +36,7 @@ function Navbar() {
         </button>
 
         <ul
-          className={`${Styles.navList} ${navOpen ? Styles.navListOpen : ''}`}
+          className={`${Styles.navList} ${navOpen ? Styles.navListOpen : ""}`}
         >
           <li className={`${Styles.navItem} ${Styles.navItemLink}`}>
             <Link to="/resources" title="Discover climate change resources">
