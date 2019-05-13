706d86e47de5451f0fe13c3bfcf70ebe4eb97f27
Paul McBride <hello@paulmcbride.net>
2019-05-13 09:22:49 +0100

    speed up typing on homepage

--- src/components/HomeIntro/index.js
+++ src/components/HomeIntro/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import GlobalStyles from '../../styles/Global.module.scss';
-import ButtonStyles from '../../styles/Buttons.module.scss';
-import Styles from './Styles.module.scss';
-import Typist from 'react-typist';
-import TypistLoop from 'react-typist-loop';
+import React, { useState, useEffect } from "react";
+import GlobalStyles from "../../styles/Global.module.scss";
+import ButtonStyles from "../../styles/Buttons.module.scss";
+import Styles from "./Styles.module.scss";
+import Typist from "react-typist";
+import TypistLoop from "react-typist-loop";
 
 const HomeIntro = () => {
   const [mounted, setMounted] = useState(false);
@@ -19,27 +19,28 @@ const HomeIntro = () => {
       <div className={`${GlobalStyles.inner} ${Styles.inner}`}>
         <div className={Styles.content}>
           <h1 className={Styles.title}>
-            We must cut global emissions in half by 2030 or face{' '}
+            We must cut global emissions in half by 2030 or face{" "}
             {!mounted ? (
               <span className={Styles.titleEmphasis}>mass extinctions</span>
             ) : (
               <TypistLoop interval={0}>
                 {[
-                  'extreme weather',
-                  'water shortages',
-                  'crop failures',
-                  'wildfires',
-                  'mass migrations',
-                  'sea level rises',
-                  'mass extinctions'
+                  "extreme weather",
+                  "water shortages",
+                  "crop failures",
+                  "wildfires",
+                  "mass migrations",
+                  "sea level rises",
+                  "mass extinctions"
                 ].map(text => (
                   <Typist
                     className={Styles.typist}
                     key={text}
                     cursor={{ show: false }}
+                    avgTypingDelay={45}
                   >
                     <span className={Styles.titleEmphasis}>{text}</span>
-                    <Typist.Backspace count={text.length} delay={2000} />
+                    <Typist.Backspace count={text.length} delay={3000} />
                   </Typist>
                 ))}
               </TypistLoop>
