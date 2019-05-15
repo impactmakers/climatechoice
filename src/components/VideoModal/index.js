8ee7e48c171bd192bdbdd3abedbc632c752fcefe
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-05-15 12:48:01 +0100

    adding iframed video

--- src/components/VideoModal/index.js
+++ src/components/VideoModal/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonStyles from "../../styles/Buttons.module.scss";
 
 export default function VideoModal() {
   return (
@@ -14,10 +15,32 @@ export default function VideoModal() {
         height: "300px",
         maxWidth: "100%",
         boxShadow: "0 1px 40px 0 rgba(50,47,68,0.2)",
-        padding: "1.5em"
+        padding: "1.5em",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
       }}
     >
-      IM A MODAL{" "}
+      <iframe
+        style={{ display: "block" }}
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/YLIzhX6pB7g"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen="true"
+      />
+
+      <p>What do you think now? </p>
+      {/* pull this out into separate component eg greenButton / can I pass props to change the size of the button too? makes it more resusable  */}
+      <button
+        className={`${ButtonStyles.btnSimple} ${ButtonStyles.btnMd} ${
+          ButtonStyles.btnGreen
+        } ${ButtonStyles.btnMobile}`}
+      >
+        Learn how you can help
+      </button>
     </div>
   );
 }
