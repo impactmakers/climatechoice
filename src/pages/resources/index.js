89e871067b9c141b84aa9af7aebba61cffb8906d
shylands <steve@shylands.com>
2019-05-23 17:00:23 +0200

    adding redirects file and some content for resources page

--- src/pages/resources/index.js
+++ src/pages/resources/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
+import { Link } from "gatsby";
 import Layout from "../../components/layout";
 import SEO from "../../components/seo";
 import GlobalStyles from "../../styles/Global.module.scss";
+import ButtonStyles from "../../styles/Buttons.module.scss";
+import Styles from "./Styles.module.scss";
 import FooterCta from "../../components/FooterCta";
 
 const ResourcesPage = () => (
@@ -14,8 +18,15 @@ const ResourcesPage = () => (
         <div className={GlobalStyles.subpageContent}>
           <h1 className={`${GlobalStyles.title} ${GlobalStyles.subpageTitle}`}>
             Resources
-          </h1>
-          <p>Content here</p>
+          </h1>{" "}
+          <p>
+            We're still working on our list of handy climate breakdown
+            resources. For now why not checkout the{" "}
+            <Link to="/#your-choices" title="Go home">
+              choices you can make today to help prevent climate breakdown
+            </Link>
+            .
+          </p>
         </div>
       </div>
     </section>
